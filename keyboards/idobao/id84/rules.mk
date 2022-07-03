4400261df0cc3f30260103eaac71756b8251e22a
Vino Rodrigues <github@m.vinorodrigues.com>
2022-07-03 10:18:23 +1000

    Add ID84

---
+++ keyboards/idobao/id84/rules.mk
@@ -0,0 +1,5 @@
+# Copyright 2022 Vino Rodrigues (@vinorodrigues)
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+RGB_MATRIX_ENABLE = yes     # Enable RGB Matrix feature
+RGB_MATRIX_DRIVER = WS2812 	# ID84 uses WS2812 driver
