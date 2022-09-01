13c9924657915b3d1a49bd297f6109c8a877454e
Vino Rodrigues <github@m.vinorodrigues.com>
2022-09-01 22:33:21 +1000

    support old via

--- keyboards/idobao/id68/id68.h
+++ keyboards/idobao/id68/id68.h
@@ -4,3 +4,9 @@
 #pragma once
 
 #include "quantum.h"
+
+#ifdef VIA_PROTOCOL_VERSION
+    // Allow VIA 1.3.1 to work on QMK >= 0.18.2
+    #undef VIA_PROTOCOL_VERSION
+    #define VIA_PROTOCOL_VERSION 0x0009
+#endif
